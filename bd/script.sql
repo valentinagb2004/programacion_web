d676f6e11818f4c512be95010e2285aa0900f435
valentinagb2004 <valentinagutierrezb2004@gmail.com>
2025-05-06 21:19:01 -0500

    se subio el insert

--- bd/script.sql
+++ bd/script.sql
@@ -1,3 +1,4 @@
+use web_noticias;
 Drop table noticias;
 
 CREATE TABLE `noticias` (
@@ -6,6 +7,21 @@ CREATE TABLE `noticias` (
   `imagen` varchar(200) NOT NULL,
   `descripcion` varchar(2000) NOT NULL,
   `categoria` varchar(50) NOT NULL,
-  `activo` varchar(50) NOT NULL,
+  `activo` varchar(1) NOT NULL,
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+insert into noticias values (
+    1, "Preguntas de la consulta popular para revivir la reforma laboral de Petro no favorecen a los trabajadores: Es egoísta",
+    "img/Petro.jpg",
+    "Expertos analizaron los 12 cuestionamientos que presentaron los ministros Armando Benedetti y Antonio Sanguino y coincidieron en que están mal diseñados, sobre todo, porque deja a un lado a los empleados informales",
+    "Farandula", 'Y',
+    "2025-05-06 20:00:00"
+);
+
+
+
+
+
+
+
